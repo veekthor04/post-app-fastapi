9deb6eb9a1436e23c2e29ea30647c5102e977ace
veekthor04 <victoradenuga04@yahoo.com>
2022-05-28 10:46:53 +0100

    added pytest to git action

--- app/config.py
+++ app/config.py
@@ -3,7 +3,6 @@ from pydantic import BaseSettings
 
 class Settings(BaseSettings):
     DATABASE_URL: str
-    TEST_DATABASE_URL: str
     SECRET_KEY: str
     ALGORITHM: str
     ACCESS_TOKEN_EXPIRE_MINUTES: int
